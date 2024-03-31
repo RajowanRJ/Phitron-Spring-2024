7bd4784c00f0352a3c90dda297c084ceb8242f0f
Rajowan <rajowanrj@gmail.com>
2024-03-31 17:43:37 +0600

    First commit

---
+++ Semester 1/Week 1/Module 1/DeclareAssignInitialize.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ()
+{
+    int Rajowan;    // Declaration
+    Rajowan = 100;  // Assigning Value
+    int Rajowan2 = 200; // Initialization 
+    
+    return 0;
+}
---
+++ Semester 1/Week 1/Module 1/Variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int Alu = 100;              
+    printf ("%d\n", Alu);
+
+    float chal = 20.555;
+    printf ("%0.2f\n", chal);
+
+    char ami = 'A';
+    printf ("%c\n", ami);
+
+    return 0;
+}
