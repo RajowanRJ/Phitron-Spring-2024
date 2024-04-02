6201190fa1f445faeb5363ea6a23a154e9350cdd
Rajowan <rajowanrj@gmail.com>
2024-04-02 06:57:28 +0600

    Added Module 2.5

---
+++ Semester 1/Week 1/Module 2.5/Problem3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    int in;
+
+    scanf ("%d", &in);
+
+    if (in % 2 != 0)
+    printf ("odd");
+
+    else
+    printf("even");
+
+    return 0;
+}
---
+++ Semester 1/Week 1/Module 2.5/Problem4.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main ()
+{
+    int in;
+    
+    scanf ("%d", &in);
+
+    if (in > 0)
+    printf ("positive");
+
+    else if (in < 0)
+    printf ("negative");
+
+    else 
+    printf ("zero");
+}
