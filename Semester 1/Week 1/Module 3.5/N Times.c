5bf04fb5c722fc550abaaed4404d7d51c27dce3a
Rajowan <rajowanrj@gmail.com>
2024-04-04 08:21:11 +0600

    Added Contest module

---
+++ Semester 1/Week 1/Module 3.5/N Times.c
@@ -0,0 +1,17 @@
+// Starting in the name of Almighty Allah
+#include <stdio.h>
+
+int main() {
+
+    int i = 0, n;
+    
+    scanf ("%d", &n);
+    
+    while (i < n) 
+    {
+        printf("I Love Practice\n");
+        i++;
+    }
+    
+    return 0;
+}
