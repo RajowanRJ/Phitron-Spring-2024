c8e82cd7c98460f2c10700689ff488c680da7150
Rajowan <rajowanrj@gmail.com>
2024-04-16 21:50:54 +0600

    Module 6.5

---
+++ Semester 1/Week 2/Module 6.5/A_Timon_and_Pumbaa.c
@@ -0,0 +1,9 @@
+// Starting with the name of Almighty Allah
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    (a - b >= 0) ? printf("%d", a - b) : printf("0");
+}
---
+++ Semester 1/Week 2/Module 6.5/K_Divisors.c
@@ -0,0 +1,15 @@
+// Starting with the name of Almighty Allah
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0) {
+            printf("%d\n", i);
+        }
+    }
+}
