f20a7e1d5cbb7e010976bd255a71ec9e88e1bcc8
Rajowan <rajowanrj@gmail.com>
2024-04-19 09:08:30 +0600

    Module 3

---
+++ Semester 1/Introduction to C/Week 1/Module 3/Print_N_Numbers.c
@@ -0,0 +1,16 @@
+// Starting in the name of Almighty Allah
+#include <stdio.h>
+
+int main ()
+{
+    int i = 1, n;
+    scanf("%d", &n);
+
+    while (i <= n) 
+    {
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
