fc91fab2d48b62778b2c8a73813d3dd03ad00dd3
Rajowan <rajowanrj@gmail.com>
2024-04-19 09:12:29 +0600

    Week 1

--- Semester 1/Week 1/Module 01/print.c
+++
@@ -1,9 +0,0 @@
-// Starting in the name of Almighty Allah
-
-#include <stdio.h>
-
-int main ()
-{
-    printf ("Hello World!");
-    return 0;
-}
--- Semester 1/Week 1/Module 3.5/I Love Practice.c
+++
@@ -1,9 +0,0 @@
-// Starting in the name of Almighty Allah
-#include <stdio.h>
-
-int main() {
-
-    printf("I Love Practice\n");
-    
-    return 0;
-}
