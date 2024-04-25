bd20c816d56696a623c6a61a8f7d7d1bf29628c0
Rajowan <rajowanrj@gmail.com>
2024-04-25 19:51:39 +0600

    Module 10.5

--- Semester 1/Introduction to C/Week 3/Module 10.5/D_Strings.c
+++ Semester 1/Introduction to C/Week 3/Module 10.5/D_Strings.c
@@ -6,22 +6,8 @@ int main ()
     char a[22], b[11], c[11], tmp;
     scanf("%s %s", a, b);
     // Saving the Value of a to another variable
-    for (int i = 0; i < strlen(a); i++)
-    {
-        c[i] = a[i];
-    }
-    
-    int length = 0;
-    while (a[length] != '\0')
-    {
-        a[length];
-        length++;
-    }
-    for (int i = 0; b[i] != '\0'; i++,length++)
-    {
-        a[length] = b[i];
-    }
-    a[length] = '\0';
+    strcpy(c, a);
+    strcat(a,b);
 
     printf("%d %d\n", strlen(c), strlen(b));
     printf("%s\n", a);
